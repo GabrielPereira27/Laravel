a3b6fe39330bb56f1f1322583183ed971a0a2239
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-19 11:07:31 +0100

    Update bemvindo.blade.php, mostranome.blade.php, and 2 more files...

---
+++ web.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/Bem-vindo', function () {
+    echo ('Olá Mundo');
+});
+
+Route::get('/', function () {
+	echo ('<h1>Olá Mundo</h1>');
+});
+
+Route::get('/bem-vindo', function(){
+	echo ('<h1>Esta é a página  que lhe dará as boas vindas </h1>');
+});
+
+Route::get('/bem-vindo/aedah', function(){
+	echo ('<h1>esta é só para o Agrupamento :) </h1>');
+});
+
+Route::get('/nome/{nome}', function ($nome) {
+	echo ('<h1>Olá '.$nome.' </h1>');
+});
+
+Route::get('/nome/{nome}/{apelido}', function ($nome, $apelido) {
+	echo ('<h1>Olá ' .$nome. ' '. $apelido. '</h1>');	
+});
+
+Route::get('/repetir/{nome}/{num}', function ($nome, $n) {
+ $numero = is_numeric ($n) ? $n :5;
+	for ($i=0; $i<$numero; $i++) { 
+	 	echo ('<h1>Olá ' .$nome.'</h1>');
+};
+return;
+}); 
+
+Route::get('/welcome/', function (){
+	return view ('bemvindo');
+});
+
+Route::get('nome/{nome}/{apelido}', function ($nome = null, $apelido = null) {
+	return view('mostranome' , [ 'nome' =>$nome, 'apelido' =>$apelido ]);
+});
+
+Route::get('/tarefas', function (){
+	$tarefas = ['Comprar senha', 'Imprimir fotocópias', 'Carregar cartão'];
+	return view ('tarefas',[ 'tarefas'=> $tarefas]);	
+});
